272e7df22e649b266d03260531aa8df962bb4269
Edward Ciccone <etpciccone@gmail.com>
2016-12-10 16:42:19 -0600

    updated start method to load and display the empty view, MyView.fxml

--- src/app/MyApp.java
+++ src/app/MyApp.java
@@ -1,13 +1,20 @@
 package app;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MyApp extends Application {
     
     @Override
-    public void start(Stage stage) {
-        
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/app/view/MyView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
